48dfbe5db8ab6b5b31b49b9692d55d5b08b51628
yoseflakew25 <80267669+yoseflakew25@users.noreply.github.com>
2026-08-05 20:33:08 +0000

    Deploying to deploy from @ yoseflakew25/dicdodd-landing-page@4e0362a820a7f044567dbd539f9b74804a7d09c8 🚀

---
+++ _next/static/chunks/app/contact/page-69744ee91016678a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[977],{3235:(e,s,_)=>{Promise.resolve().then(_.bind(_,5735))}},e=>{var s=s=>e(e.s=s);e.O(0,[355,616,309,735,441,517,358],()=>s(3235)),_N_E=e.O()}]);
